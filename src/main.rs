36d0fcf90a31698efdf86e938809486592501e9e
Eugen Patru <eugen.ptr@gmail.com>
2024-07-30 23:14:15 +0300

    feat: add polars deps and read dataframe from csv feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use polars_core::prelude::*;
+use polars_io::prelude::*;
+
+fn read_csv(file: &str) -> PolarsResult<DataFrame> {
+    CsvReadOptions::default()
+            .with_has_header(true)
+            .try_into_reader_with_file_path(Some(file.into()))?
+            .finish()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data_file = "output.csv";
+
+    let df : PolarsResult<DataFrame> = read_csv(data_file);
+    println!("{:?}", df);
 }
