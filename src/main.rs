04bc94af9999bb1c59a8b66ac09c3da9b00229e1
Eugen Patru <eugen.ptr@gmail.com>
2024-08-09 11:49:54 +0300

    wip: adding ndarray-linalg breaks build

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,48 @@ use linfa::prelude::*;
 use linfa::traits::Fit;
 use linfa_linear::LinearRegression;
 use ndarray::prelude::*;
+use ndarray::{stack, Axis, Array1, Array2};
+// use ndarray_linalg::LeastSquaresSvd;
 use polars::prelude::*;
 use anyhow::{anyhow as err, Error};
+use std::f64::consts::PI;
 
 fn read_csv(file: &str) -> PolarsResult<DataFrame> {
     CsvReadOptions::default()
         .with_has_header(true)
         .try_into_reader_with_file_path(Some(file.into()))?
         .finish()
+}
+
+fn season_matrix(t: Array1<f64>) -> Array2<f64> {
+    let sin_2pi_24 = t.mapv(|time| (2.0 * PI * time / 24.0).sin());
+    let cos_2pi_24 = t.mapv(|time| (2.0 * PI * time / 24.0).cos());
+    let sin_4pi_24 = t.mapv(|time| (4.0 * PI * time / 24.0).sin());
+    let cos_4pi_24 = t.mapv(|time| (4.0 * PI * time / 24.0).cos());
+    let sin_8pi_24 = t.mapv(|time| (8.0 * PI * time / 24.0).sin());
+    let cos_8pi_24 = t.mapv(|time| (8.0 * PI * time / 24.0).cos());
+    let sin_2pi_24_7 = t.mapv(|time| (2.0 * PI * time / (24.0 * 7.0)).sin());
+    let cos_2pi_24_7 = t.mapv(|time| (2.0 * PI * time / (24.0 * 7.0)).cos());
+    let sin_4pi_24_7 = t.mapv(|time| (4.0 * PI * time / (24.0 * 7.0)).sin());
+    let cos_4pi_24_7 = t.mapv(|time| (4.0 * PI * time / (24.0 * 7.0)).cos());
+    let sin_8pi_24_7 = t.mapv(|time| (8.0 * PI * time / (24.0 * 7.0)).sin());
+    let cos_8pi_24_7 = t.mapv(|time| (8.0 * PI * time / (24.0 * 7.0)).cos());
+
+    stack![
+        Axis(1),
+        sin_2pi_24,
+        cos_2pi_24,
+        sin_4pi_24,
+        cos_4pi_24,
+        sin_8pi_24,
+        cos_8pi_24,
+        sin_2pi_24_7,
+        cos_2pi_24_7,
+        sin_4pi_24_7,
+        cos_4pi_24_7,
+        sin_8pi_24_7,
+        cos_8pi_24_7
+    ]
 }
 
 fn main() -> Result<(), Error> {
@@ -138,9 +172,10 @@ fn main() -> Result<(), Error> {
         let time_index_series = Series::new("time_index", (0..df.height() as i64).into_iter());
         df.with_column(time_index_series)?;
 
-        let ones = Array::<f64, Ix1>::ones((df.height() as usize));
-        let x = ndarray::stack![
-            ndarray::Axis(1),
+        let ones = Array::<f64, Ix1>::ones(
+            df.height() as usize);
+        let x = stack![
+            Axis(1),
             Array::from(
                 df["time_index"]
                     .cast(&DataType::Float64)?
@@ -171,6 +206,42 @@ fn main() -> Result<(), Error> {
             "detrended_log_base_fee",
             df["log_base_fee"].f64()? - df["trend"].f64()?,
         ))?;
+
+        // Seasonality modelling amd removal from the detrended log base fee
+        // ===============================================================================================
+
+        let start_date_value = df
+            .column("date")?
+            .datetime()?
+            .get(0)
+            .ok_or_else(|| err!("No row 0 in the date column"))?;
+        let start_date = DateTime::from_timestamp(start_date_value / 1000, 0)
+            .ok_or_else(|| err!("Can't calculate the start date"))?;
+
+        
+
+        // Optimisation tip: this is same as the time_index, so we can reuse it
+        let t_series: Vec<f64> = df
+            .column("date")?
+            .datetime()?
+            .into_iter()
+            .map(|opt_date| {
+                opt_date.map_or(0.0, |date| {
+                    (DateTime::from_timestamp(date / 1000, 0).unwrap() - start_date).num_seconds() as f64 / 3600.0
+                })
+            })
+            .collect();
+
+        df.with_column(Series::new("t", t_series))?;
+
+        let t_array = df["t"].f64()?.to_ndarray()?.to_owned();
+        let c = season_matrix(t_array);
+
+        // let detrended_log_base_fee_array = df["detrended_log_base_fee"].f64()?.to_ndarray()?.to_owned();
+        // let season_param = c.least_squares(&detrended_log_base_fee_array)?.solution;
+        // let season = c.dot(&season_param);
+        // let de_seasonalised_detrended_log_base_fee = df["detrended_log_base_fee"].f64()?.to_ndarray()?.to_owned() - season;
+        // df.with_column(Series::new("de_seasonalized_detrended_log_base_fee", de_seasonalised_detrended_log_base_fee.to_vec()))?;
 
         println!("{:?}", df);
         break;
